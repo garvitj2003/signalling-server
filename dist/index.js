98885fde6b2cb82a4792236f65c1376e8010eebb
Garvit <garvitj2003@gmail.com>
2025-05-30 12:51:50 +0530

    cors prod up

--- dist/index.js
+++ dist/index.js
@@ -7,12 +7,22 @@ const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
 const socket_io_1 = require("socket.io");
 const cors_1 = __importDefault(require("cors"));
+const allowedOrigins = [
+    "http://localhost:3000", // for dev
+    "https://rekor.vercel.app", // deployed client
+];
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: allowedOrigins,
+    credentials: true, // Allow credentials if needed
+}));
 const server = http_1.default.createServer(app);
+// enabled cors for all routes for now to be changed in prod
 const io = new socket_io_1.Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: allowedOrigins,
+        methods: ["GET", "POST"],
+        credentials: true, // Allow credentials if needed
     },
 });
 // Type-safe Maps for rooms and users
